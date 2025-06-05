56a1c4f1240528703304f07b73e92810a3281e0f
moasama6 <dev@retroagenda.com>
2025-06-05 13:05:25 -0700

    Added Healthbar and Score UI

--- main.gd
+++ main.gd
@@ -1,9 +1,26 @@
 extends Node
 
 var dynamicObjectSpeed : int = 700
+var healthDecreaseSpeed : int = 3
+var health : float = 100
+var score : float = 0
+var formattedScore : String = str(score)
+var decimalIndex = formattedScore.find(".")
+
 
 func _process(delta: float) -> void:
 	for dynamic_object in get_tree().get_nodes_in_group("DynamicObject"):
 		dynamic_object.position.x -= delta * dynamicObjectSpeed
+	if health > 0:
+		health -= delta * healthDecreaseSpeed
+		$UI/HealthBar.value = health
+		
+	score += delta
+	formattedScore = str(score)
+	formattedScore = formattedScore.left(decimalIndex + 3)
+	$UI/HealthBar/ScoreLabel.text = formattedScore
+	#else:
+		#print("Game Over")
+	
 		
 		
